5fdaf263121285d0f54871ee72510fe482561472
siravich1988 <siravichker@gmail.com>
2026-03-20 14:29:03 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,18 @@
 import './globals.css';
+
+export const metadata = {
+  title: 'GA Pro System',
+  description: 'ระบบแจ้งซ่อมและจองส่วนกลาง',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="th">
+      <body className="bg-[#121212]">{children}</body>
+    </html>
+  );
+}
